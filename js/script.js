1e3b26256d8a96304b56991117e3e7ead160f9ed
unknown <gustavo.dantas.marim@outlook.com>
2022-05-01 13:39:11 -0300

    Finalição da sessão Discussões

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// SECTION => RESUMO
 const btnVerMais = document.querySelector('.btn-ver-mais');
 
 const paragrafoOculto = document.querySelectorAll('.p-oculto');
@@ -16,12 +17,26 @@ const btnCriarTopico = document.querySelector('.btn-criar-topico');
 btnCriarTopico.addEventListener('click', () => {
   const form = document.querySelector('.form-criar-topico');
   form.classList.toggle('form-criar-topico-oculto');
+  const divContainerAtivado = document.querySelector('.container-ativado');
+  divContainerAtivado.classList.toggle('display-none');
 });
 
+//SECTION => DISCUSSÕES: CRIAR TOPICO
+const btnEnviarTopico = document.querySelector('.btn-enviar-topico');
+btnEnviarTopico.addEventListener('click', (e) => {
+  e.preventDefault();
+  const form = document.querySelector('.form-criar-topico');
+  form.classList.toggle('form-criar-topico-oculto');
+  btnCriarTopico.classList.toggle('display-none');
+  const divTopicoEnviado = document.querySelector('.topico-enviado');
+  divTopicoEnviado.classList.toggle('display-none');
+});
+
+
+// SECTION => DISCUSSÕES ACCORDION => PERGUNTAS
 const topicoAberto = document.querySelector('.topico-aberto');
 const topicoAccordion = document.querySelector('.topico-accordion');
 const abrirTopico = document.querySelector('.accordion');
-// implementar lógica para abrir o topico
 
 abrirTopico.addEventListener('click', () => {
   topicoAberto.classList.toggle('display-none');
